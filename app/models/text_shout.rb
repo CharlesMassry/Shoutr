9c952eea7bc5c791cbbae8473d11978a93d03f6a
Charlie Massry <charlesdmassry@gmail.com>
2014-07-21 09:12:53 -0400

    simplified search method

--- app/models/text_shout.rb
+++ app/models/text_shout.rb
@@ -2,6 +2,6 @@ class TextShout < ActiveRecord::Base
   has_one :shout, as: :content, dependent: :destroy
 
   def self.search(search)
-    where(["content ILIKE :text", { text: "%#{search}%" }])
+    where("content ILIKE ?", "%#{search}%")
   end
 end
